e326cad384a490eb0ec5d22b56794a4135b6274a
Ryan Augustyn <jraugustyn07@gmail.com>
2023-03-30 16:55:51 -0500

    completed tutorial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); //connects to html element with 'root' in ID
 root.render(<App />); //renders in the index.html file
